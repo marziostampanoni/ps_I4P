64becd07e70bdf71d8c09491fd241901f7782593
rezart <rezart.lohja@gmail.com>
2017-01-12 19:36:46 +0100

    ..

---
+++ moodle31/local/courseseditor/lang/en/local_courseseditor.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rezart Lohja
+ * Date: 12.01.17
+ * Time: 19:33
+ */
+
+$string['pluginname'] = 'Courses editor';
+$string['pluginname_desc'] = 'This plugin ...';
+
+/**
+ * Settings
+ */
+
+$string['usi_host_name'] = 'USI Host';
+$string['usi_host_description'] = 'The address of the USI server';
+$string['supsi_host_name'] = 'USI Host';
+$string['supsi_host_description'] = 'The address of the USI server';
+
--- moodle31/local/courseseditor/version.php
+++ moodle31/local/courseseditor/version.php
@@ -8,7 +8,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2017011000;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2017011000;        // Requires this Moodle version
+$plugin->requires  = 2014050800;        // Requires this Moodle version
 $plugin->component = 'local_courseseditor'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity  = MATURITY_STABLE;
 
