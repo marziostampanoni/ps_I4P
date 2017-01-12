c9a860dc839107c9293055affd1607d71bf30f9b
rezart <rezart.lohja@gmail.com>
2017-01-12 18:48:40 +0100

    ..

---
+++ moodle31/local/courseseditor/version.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package    local_netidsync
+ * @copyright  2014 Guglielmo Fachini {@link http://fachini.net}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2017011000;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2017011000;        // Requires this Moodle version
+$plugin->component = 'local_courseseditor'; // Full name of the plugin (used for diagnostics)
+$plugin->maturity  = MATURITY_STABLE;
+
