4eff5703762419bf1c63eefb1d0d30be6b2b3135
rezart <rezart.lohja@gmail.com>
2017-01-12 20:02:56 +0100

    ..

--- moodle31/local/courseseditor/lib.php
+++ moodle31/local/courseseditor/lib.php
@@ -8,15 +8,15 @@
 
 
 function local_courseseditor_extend_navigation(global_navigation $navigation) {
-    global $CFG, $PAGE;
-    $previewnode = $PAGE->navigation->add(get_string('pluginname','local_courseseditor'), new moodle_url('/local/courseseditor/start.php'), navigation_node::TYPE_CONTAINER);;
+    global $PAGE;
+    $node = $PAGE->navigation->add(get_string('pluginname','local_courseseditor'), new moodle_url('/local/courseseditor/start.php'), navigation_node::TYPE_CONTAINER);;
 
-    $thingnode = $previewnode->add(get_string('new_courses','local_courseseditor'), new moodle_url('/local/courseseditor/new_courses.php'));
-    $thingnode->make_active();
-    $thingnode = $previewnode->add(get_string('clone_courses','local_courseseditor'), new moodle_url('/local/courseseditor/clone_courses.php'));
-    $thingnode->make_active();
-    $thingnode = $previewnode->add(get_string('delete_courses','local_courseseditor'), new moodle_url('/local/courseseditor/delete_courses.php'));
-    $thingnode->make_active();
+    $node1 = $node->add(get_string('new_courses','local_courseseditor'), new moodle_url('/local/courseseditor/new_courses.php'));
+    $node1->make_active();
+    $node2 = $node->add(get_string('clone_courses','local_courseseditor'), new moodle_url('/local/courseseditor/clone_courses.php'));
+    $node2->make_active();
+    $node3 = $node->add(get_string('delete_courses','local_courseseditor'), new moodle_url('/local/courseseditor/delete_courses.php'));
+    $node3->make_active();
 }
 
 function local_courseseditor_extend_settings_navigation($settingsnav, $context) {
